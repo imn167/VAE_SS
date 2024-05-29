888bb12fe576c4aa64d90bf7f9568fe9095e4fa8
imn167 <imn167.tb@gmail.com>
2024-05-29 17:23:03 +0200

    GoM avec distribution bimodale

--- function/VAE_GoM.py
+++ function/VAE_GoM.py
@@ -8,20 +8,22 @@ import time
 
 ##### Creation of the Sampling class ########
 
-class MoGPrior(layers.Layer):
+class MoGPrior(tf.keras.Model):
   def __init__(self, latent_dim, num_components, **kwargs):
     super(MoGPrior, self).__init__(**kwargs)
     self.latent_dim = latent_dim
     self.num_components = num_components
 
     # means and log variances (trainable parameters)
-    self.means = tf.Variable(tf.random.normal(shape=(num_components, latent_dim)), 
+    self.means = tf.Variable(tf.random.normal(shape=(num_components, latent_dim)),
                             trainable=True, name='means')
     self.logvars = tf.Variable(tf.random.normal(shape=(num_components, latent_dim)),
                                trainable=True, name='logvars')
 
     # mixing weights (trainable parameter)
-    self.w = tf.Variable(tf.ones(shape=(1,num_components)), trainable=True)
+    self.w = tf.Variable(tf.random.normal(shape=(1,num_components)), trainable=True)
+
+    self.loss_tracker = tf.keras.metrics.Mean(name = 'loss')
 
   def get_params(self):
     return self.means, self.logvars, self.w
@@ -36,34 +38,47 @@ class MoGPrior(layers.Layer):
     indexes = (tf.random.categorical(tf.math.log(w), batch_size))[0]
 
     # sample from chosen components
-    z = tf.map_fn(fn= lambda indx : means[indx] + tf.random.normal(shape= (1,2)) * tf.exp(0.5*logvars[indx]), 
+    z = tf.map_fn(fn= lambda indx : means[indx] + tf.random.normal(shape= (1,2)) * tf.exp(0.5*logvars[indx]),
           elems= indexes,
           dtype=tf.float32)
     z = tf.squeeze(z)
 
     return z
-  #compute the log_density of each gaussian at the point z 
+  #compute the log_density of each gaussian at the point z
   def log_normal_diag(self, z, mean, logvar):
-     nn_exp = -0.5*( tf.math.log(2.0*np.pi) + logvar) 
+     nn_exp = -0.5*( tf.math.log(2.0*np.pi) + logvar)
      exp = -0.5* (z-mean)**2 * (tf.exp(-logvar))
-     return tf.reduce_sum(nn_exp + exp, axis = -1) 
-  
-  def log_prob(self, z):
-     #getting means and vars of the gausiian mixture 
-     means, logvars, w = self.get_params() 
+     return tf.reduce_sum(nn_exp + exp, axis = -1)
 
-     #normalising the weight with the softmax transformation 
+  def log_prob(self, z):
+     #getting means and vars of the gausiian mixture
+     means, logvars, w = self.get_params()
+
+     #normalising the weight with the softmax transformation
      w = tf.transpose(tf.nn.softmax(w, axis=1))
-     #reshape for broadcast 
+     #reshape for broadcast
      z =  tf.expand_dims(z, axis=0) #1 x batch x latent_dim
      means = tf.expand_dims(means, axis=1) #num_compo x 1 x latent_dim
      logvars = tf.expand_dims(logvars, axis=1) #num_compo x 1 x latent_dim
 
-     #we compute the log of each gaussian for each z 
+     #we compute the log of each gaussian for each z
      log_p = self.log_normal_diag(z, means, logvars) + tf.math.log(w) #num_compo x batch
      prob = tf.reduce_logsumexp(tf.squeeze(log_p), axis=0) #log(sum(exp())) #batch
      return prob
 
+  def train_step(self, data):
+
+    with tf.GradientTape() as tape :
+      log_likelyhood = self.log_prob(data) #data is latent space sampling
+      loss = tf.reduce_mean(-log_likelyhood)
+
+    grads = tape.gradient(loss, self.trainable_variables)
+    self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+    self.loss_tracker.update_state(loss)
+    return {m.name: m.result() for m in self.metrics}
+
+
+###################################################
   
 class Sampling(layers.Layer):
     def call(self, inputs):
@@ -75,13 +90,14 @@ class Sampling(layers.Layer):
     
 ##### Creation of the Encoder Class #####
 class Encoder(layers.Layer):
-  def __init__(self, input_dim, latent_dim, **kwargs):
+  def __init__(self, input_dim, latent_dim, training, **kwargs):
     super(Encoder, self).__init__(name='encoder', **kwargs)
+    self.act = layers.LeakyReLU(alpha = .3)
     self.enc1 = layers.Dense(input_dim, activation='relu')
     self.enc2 = layers.Dense(64, activation = 'relu')
     self.enc3 = layers.Dense(32, activation = 'relu')
     self.mean_z = layers.Dense(latent_dim, name = 'z_mean')
-    self.logvar_z = layers.Dense(latent_dim, name = 'z_log_var')
+    self.logvar_z = layers.Dense(latent_dim, name = 'z_log_var', trainable = training)
     self.sampling = Sampling()
 
   def call(self, inputs):
@@ -98,13 +114,13 @@ class Encoder(layers.Layer):
 ############## Creation of the Decoder Class #################
 class Decoder(layers.Layer):
 
- def __init__(self,input_dim, latent_dim, **kwargs):
+ def __init__(self,input_dim, latent_dim,training, **kwargs):
     super(Decoder, self).__init__(name='decoder', **kwargs)
     self.dec1 = layers.Dense(latent_dim, activation='relu' )
     self.dec2 = layers.Dense(32, activation='relu')
     self.dec3 = layers.Dense(64, activation='relu')
     self.out = layers.Dense(input_dim, name = 'x_out')
-    self.x_log_var = layers.Dense(input_dim, name = 'x_log_var')
+    self.x_log_var = layers.Dense(input_dim, name = 'x_log_var', trainable = training)
 
  def call(self, z):
    z = self.dec1(z)
@@ -112,6 +128,37 @@ class Decoder(layers.Layer):
    z = self.dec3(z)
    return self.out(z), self.x_log_var(z)
  
+ ############## Class Auto-Encoder ################
+class AutoEncoder(tf.keras.Model):
+  def __init__(self, encoder,decoder, **kwargs):
+    super(AutoEncoder, self).__init__(name='auto-encoder', **kwargs)
+    self.encoder = encoder
+    self.decoder = decoder
+
+    self.loss_tracker = tf.keras.metrics.Mean(name = 'loss')
+
+  def call(self, inputs):
+    z_mean, _, _ = self.encoder(inputs)
+    x_mean, _ = self.decoder(z_mean)
+    return z_mean, x_mean 
+
+  def train_step(self, data):
+
+
+    with tf.GradientTape() as tape :
+
+          _, x_mean= self(data)
+            #we compute the first loss : the log-likelyhood
+          total_loss = tf.reduce_mean(tf.reduce_sum(tf.pow(data-x_mean, 2), axis =1))
+            #scale_x = tf.exp(x_log_var) #variance
+            #log_pdf = 0.5 * tf.reduce_sum(tf.pow(data-reconstruction, 2) / scale_x + x_log_var, axis = 1) #-log_pdf because we want to maximise it (SGD aim to minimize in keras)
+            #total_loss =  tf.reduce_mean(log_pdf) #tf.multiply(log_pdf, y)
+
+    grads = tape.gradient(total_loss, self.trainable_weights)
+    self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+    self.loss_tracker.update_state(total_loss)
+    return {m.name: m.result() for m in self.metrics}
     
 ##################### Creation of subclass od Model : VAE ########################
 ## We can see the subclass VAE as a Keras Model therefore it has the several method as fit and compile 
